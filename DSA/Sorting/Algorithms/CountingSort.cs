167b5bd498da0ccde734fcafc07ea0d3c868d6a5
Daavipf <davi.pferreira2705@gmail.com>
2025-07-27 19:23:53 -0300

    Refactor CountingSort

--- DSA/Sorting/Algorithms/CountingSort.cs
+++ DSA/Sorting/Algorithms/CountingSort.cs
@@ -4,11 +4,29 @@ public class CountingSort : Sorting
 {
     public void Sort(int[] array)
     {
-        if (array.Length <= 0)
+        if (array.Length > 0)
         {
-            return;
+            int[] countingArray = PopulateCountingArray(array);
+
+            WriteSortedValuesToArray(array, countingArray);
+        }
+    }
+
+    private int[] PopulateCountingArray(int[] array)
+    {
+        int greater = FindGreaterElement(array);
+
+        int[] countingArray = new int[greater + 1];
+        for (int k = 0; k < array.Length; k++)
+        {
+            countingArray[array[k]]++;
         }
 
+        return countingArray;
+    }
+
+    private int FindGreaterElement(int[] array)
+    {
         int greater = array[0];
         for (int i = 1; i < array.Length; i++)
         {
@@ -18,12 +36,11 @@ public class CountingSort : Sorting
             }
         }
 
-        int[] countingArray = new int[greater + 1];
-        for (int k = 0; k < array.Length; k++)
-        {
-            countingArray[array[k]]++;
-        }
+        return greater;
+    }
 
+    private void WriteSortedValuesToArray(int[] array, int[] countingArray)
+    {
         int index = 0;
         for (int j = 0; j < countingArray.Length; j++)
         {
